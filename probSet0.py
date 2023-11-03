f9a60f4b5c6fbb8d2af6ddb401ece79521f57a92
Just-Jasmine <doyoulikejas64@gmail.com>
2023-11-03 15:13:36 -0400

    Add probSet0.py

---
+++ probSet0.py
@@ -0,0 +1,10 @@
+import numpy
+
+x = input("Please enter a number for x: ")
+y = input("Please enter a number for y: ")
+
+x = int(x)
+y = int(y)
+
+print("x**y = ", x**y)
+print("log(x) = ", numpy.log2(x))
